a7af2c2631eaa2611ea83dedb01463a277b722b9
F1D022156_REGIA PUSPA AMARANTHI <regiaamaranthi@gmail.com>
2024-05-20 19:00:32 +0800

    progress

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="width=device-width, initial-scale=1.0">
+    <title>UnramGrad Career Guide</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Navbar start -->
+    <nav class="navbar">
+        <a href="#" class="navbar-logo">UnramGrad Career Guide</a>
+
+        <div class="navbar-nav">
+            <a href="#">Home</a>
+            <a href="#findjob">Find Job</a>
+            <a href="#employer">Employers</a>
+            <a href="#candidate">Candidates</a>
+        </div>
+
+        <div class="navbar-extra">
+            <a href="#" id="search"><i data-feather="search"></i></a>
+            <a href="#" id="phone-call"><i data-feather="phone-call"></i></a>
+            <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
+        </div>
+        
+    </nav>
+    <!-- Navbar end -->
+
+    <!-- Feather Icons -->
+    <script>feather.replace()</script>
+     
+</body>
+</html>
