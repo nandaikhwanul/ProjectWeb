987ff85ce46ef2a71c30c15c387aeb38a3dc8258
F1D022156_REGIA PUSPA AMARANTHI <regiaamaranthi@gmail.com>
2024-06-03 12:03:21 +0800

    update

--- signin.php
+++ signin.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
 $servername = "localhost";
-$dbusername = "root"; // Change this to your database username
-$dbpassword = ""; // Change this to your database password
+$dbusername = "root"; 
+$dbpassword = ""; 
 $dbname = "unramgradcg";
 
-// Create connection
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -17,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT password FROM users WHERE username = ?";
+    $sql = "SELECT password FROM user WHERE username = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $username);
     $stmt->execute();
@@ -27,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($stored_password && password_verify($password, $stored_password)) {
         $_SESSION['username'] = $username;
         header("Location: dashboard.php");
-        exit(); // Keluar dari skrip setelah mengalihkan pengguna
+        exit(); 
     } else {
         echo "Invalid username or password";
     }    
